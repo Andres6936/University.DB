1fc2224b6d221cfc7997b84f9d33fa96f4866034
Joan Andrés <andres6936@live.com>
2021-06-09 13:32:43 -0500

    Collection Professor Mongo

---
+++ main.js
@@ -0,0 +1,27 @@
+const {MongoClient} = require("mongodb");
+
+const client = new MongoClient('mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+async function run() {
+    try {
+        // Connect the client to the server
+        await client.connect();
+        // Establish and verify connection
+        const db = await client.db("University");
+        const professor = await db.collection('Professor');
+        const result = await professor.find({}).project({_id: 0, Name: 1});
+
+        while (await result.hasNext()) {
+            console.log(await result.next());
+        }
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
