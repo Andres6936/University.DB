c4067eb1e15f422ae84ba41f0617820d07f4c6f1
Joan Andrés <andres6936@live.com>
2021-06-11 12:42:14 -0500

    Added documentation

--- src/main/js/scrapper/WebScrappingService.mjs
+++ src/main/js/scrapper/WebScrappingService.mjs
@@ -1,6 +1,13 @@
 import cheerio from "cheerio";
 import {IScrapperService} from './IScrapperService.mjs'
 
+/**
+ * Cheerio parses markup and provides an API for traversing/manipulating the
+ * resulting data structure. It does not interpret the result as a web browser
+ * does. Specifically, it does not produce a visual rendering, apply CSS, load
+ * external resources, or execute JavaScript. This makes Cheerio much, much
+ * faster than other solutions.
+ */
 export class WebScrappingService extends IScrapperService {
     #instance = undefined;
 
