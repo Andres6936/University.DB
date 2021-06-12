34d2faed52c0fa6e41d75847c4b01858498cd525
Joan Andrés <andres6936@live.com>
2021-06-12 12:37:47 -0500

    Implemented TranslationService

--- src/main/js/TranslationService.mjs
+++ src/main/js/TranslationService.mjs
@@ -1,3 +1,23 @@
-class TranslationService {
+import translate from "@iamtraction/google-translate";
 
+export class TranslationService {
+
+    /**
+     * Translate the key and the value of pair.
+     * @param pair {Pair} The Pair object.
+     */
+    async static translatePair(pair) {
+        pair.first = await translate(pair.first).text;
+        pair.second = await translate(pair.second).text;
+    }
+
+    /**
+     * Translate a array of Pair elements.
+     * @param array {Array<Pair>} Array of Pair's.
+     */
+    async static translateArrayPair(array) {
+        for (const pair of array) {
+            await this.translatePair(pair);
+        }
+    }
 }
