a8836fc67b8120e1ee93aee787ee30db450a9e80
Joan Andrés <andres6936@live.com>
2021-06-12 15:46:35 -0500

    Normalize string

--- main.mjs
+++ main.mjs
@@ -4,6 +4,7 @@ import translate from "@iamtraction/google-translate";
 import {WebScrappingService} from "./src/main/js/scrapper/WebScrappingService.mjs";
 import {KeyValueNodeExtractor} from "./src/main/js/scrapper/KeyValueNodeExtractor.mjs";
 import {TranslationService} from "./src/main/js/TranslationService.mjs";
+import {NormalizeStringService} from "./src/main/js/NormalizeStringService.mjs";
 
 const url = 'http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000494089';
 
@@ -19,6 +20,11 @@ async function translateObject(object) {
 
 function extractPersonalInformation(node) {
     let nodes = new KeyValueNodeExtractor(node).toArray();
+    for (const node of nodes) {
+        let value = NormalizeStringService.removeExtraSpaces(node.second)
+        value = NormalizeStringService.removeLineBreak(value);
+        node.second = value;
+    }
     new TranslationService().translateArrayPair(nodes)
         .then(result => {
             for (const object of result) {
