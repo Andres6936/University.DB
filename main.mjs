20ad2067045ab09dd38997af92cb93309a1d273e
Joan Andrés <andres6936@live.com>
2021-06-13 12:48:25 -0500

    Export App class

--- main.mjs
+++ main.mjs
@@ -1,5 +1,6 @@
 import axios from "axios";
 import cheerio from 'cheerio';
+import {App} from "./src/main/js/App.mjs";
 import {KeyValueNodeExtractor} from "./src/main/js/scrapper/KeyValueNodeExtractor.mjs";
 import {TranslationService} from "./src/main/js/TranslationService.mjs";
 import {NormalizeStringService} from "./src/main/js/NormalizeStringService.mjs";
@@ -57,62 +58,63 @@ function extractLanguages() {
 
 }
 
-axios.get(url, {responseEncoding: 'latin1'})
-    .then(response => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const tableInfo = $('tr');
-        for (const table of tableInfo) {
-            //@type {string} The text of node
-            const textNode = $(table).text();
-            if (textNode.includes('Par evaluador reconocido por Minciencias')) {
-                extractPersonalInformation(table);
-            } else if (textNode.includes('Redes sociales académicas')) {
-
-            } else if (textNode.includes('Identificadores de autor')) {
-
-            } else if (textNode.includes('Formación Académica')) {
-                extractAcademicInformation(table);
-            } else if (textNode.includes('Formación Complementaria')) {
-
-            } else if (textNode.includes('Experiencia profesional')) {
-
-            } else if (textNode.includes('Áreas de actuación')) {
-
-            } else if (textNode.includes('Idiomas')) {
-
-            } else if (textNode.includes('Líneas de investigación')) {
-
-            } else if (textNode.includes('Reconocimientos')) {
-
-            } else if (textNode.includes('Cursos de corta duración')) {
-
-            } else if (textNode.includes('Trabajos dirigidos/tutorías')) {
-
-            } else if (textNode.includes('Jurado en comités de evaluación')) {
-
-            } else if (textNode.includes('Participación en comités de evaluación')) {
-
-            } else if (textNode.includes('Par evaluador')) {
-
-            } else if (textNode.includes('Eventos científicos')) {
-
-            } else if (textNode.includes('Redes de conocimiento especializado')) {
-
-            } else if (textNode.includes('Espacios de participación ciudadana')) {
-
-            } else if (textNode.includes('Artículos')) {
-
-            } else if (textNode.includes('Capitulos de libro')) {
-
-            } else if (textNode.includes('Documentos de trabajo')) {
-
-            } else if (textNode.includes('Softwares')) {
-
-            }
-        }
-    })
-    .catch(console.error);
+//
+// axios.get(url, {responseEncoding: 'latin1'})
+//     .then(response => {
+//         const html = response.data;
+//         const $ = cheerio.load(html);
+//         const tableInfo = $('tr');
+//         for (const table of tableInfo) {
+//             //@type {string} The text of node
+//             const textNode = $(table).text();
+//             if (textNode.includes('Par evaluador reconocido por Minciencias')) {
+//                 extractPersonalInformation(table);
+//             } else if (textNode.includes('Redes sociales académicas')) {
+//
+//             } else if (textNode.includes('Identificadores de autor')) {
+//
+//             } else if (textNode.includes('Formación Académica')) {
+//                 extractAcademicInformation(table);
+//             } else if (textNode.includes('Formación Complementaria')) {
+//
+//             } else if (textNode.includes('Experiencia profesional')) {
+//
+//             } else if (textNode.includes('Áreas de actuación')) {
+//
+//             } else if (textNode.includes('Idiomas')) {
+//
+//             } else if (textNode.includes('Líneas de investigación')) {
+//
+//             } else if (textNode.includes('Reconocimientos')) {
+//
+//             } else if (textNode.includes('Cursos de corta duración')) {
+//
+//             } else if (textNode.includes('Trabajos dirigidos/tutorías')) {
+//
+//             } else if (textNode.includes('Jurado en comités de evaluación')) {
+//
+//             } else if (textNode.includes('Participación en comités de evaluación')) {
+//
+//             } else if (textNode.includes('Par evaluador')) {
+//
+//             } else if (textNode.includes('Eventos científicos')) {
+//
+//             } else if (textNode.includes('Redes de conocimiento especializado')) {
+//
+//             } else if (textNode.includes('Espacios de participación ciudadana')) {
+//
+//             } else if (textNode.includes('Artículos')) {
+//
+//             } else if (textNode.includes('Capitulos de libro')) {
+//
+//             } else if (textNode.includes('Documentos de trabajo')) {
+//
+//             } else if (textNode.includes('Softwares')) {
+//
+//             }
+//         }
+//     })
+//     .catch(console.error);
 
 (async () => {
     await new App().startUp()
--- src/main/js/App.mjs
+++ src/main/js/App.mjs
@@ -1,5 +1,8 @@
-class App {
-    async startUp() {
+import axios from "axios";
 
+export class App {
+    async startUp() {
+        const html = (await axios.get('http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000494089', {responseEncoding: 'latin1'})).data
+        console.log(html)
     }
 }
