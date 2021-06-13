903f3167e32868f1be4f1c84eba661e470d6cd97
Joan Andrés <andres6936@live.com>
2021-06-13 13:03:08 -0500

    Use of configuration object

--- src/main/js/App.mjs
+++ src/main/js/App.mjs
@@ -4,6 +4,12 @@ import {AcademicInformation} from "./scrapper/AcademicInformation.mjs";
 import {PersonalInformation} from "./scrapper/PersonalInformation.mjs";
 
 export class App {
+    #translateObjects = false
+
+    constructor(configurations) {
+        this.#translateObjects = configurations.translateObjects || false;
+    }
+
     async startUp() {
         const html = (await axios.get('http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000494089', {responseEncoding: 'latin1'})).data
         const webScrapper = new WebScrappingService(html);
