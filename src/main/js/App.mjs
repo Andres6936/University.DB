44f01e1fd468d90dfbd611c94f472d14bb68942c
Joan Andrés <andres6936@live.com>
2021-06-13 13:24:27 -0500

    Added method printNodes

--- src/main/js/App.mjs
+++ src/main/js/App.mjs
@@ -10,6 +10,12 @@ export class App {
         this.#translateObjects = configurations?.translateObjects ?? false;
     }
 
+    static #printNodes(nodes) {
+        for (const node of nodes) {
+            console.log(node);
+        }
+    }
+
     async startUp() {
         const html = (await axios.get('http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000494089', {responseEncoding: 'latin1'})).data
         const webScrapper = new WebScrappingService(html);
@@ -19,8 +25,10 @@ export class App {
             const text = webScrapper.parsePage(table).toText();
             if (text.includes('Par evaluador reconocido por Minciencias')) {
                 const nodes = new PersonalInformation().start(table);
+                App.#printNodes(nodes);
             } else if (text.includes('Formación Académica')) {
                 const nodes = new AcademicInformation().start(table);
+                App.#printNodes(nodes);
             }
         }
     }
