990d5c5ed4a5b7a5d118bea773ccf0017ce3608c
Joan Andrés <andres6936@live.com>
2021-06-13 12:53:17 -0500

    Prepare the method StartUp

--- src/main/js/App.mjs
+++ src/main/js/App.mjs
@@ -1,8 +1,13 @@
 import axios from "axios";
+import {WebScrappingService} from "./scrapper/WebScrappingService.mjs";
 
 export class App {
     async startUp() {
         const html = (await axios.get('http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000494089', {responseEncoding: 'latin1'})).data
-        console.log(html)
+        const webScrapper = new WebScrappingService(html);
+        const tables = webScrapper.getElementsBySelector('tr');
+        for (const table of tables) {
+            const text = webScrapper.parsePage(table).toText();
+        }
     }
 }
