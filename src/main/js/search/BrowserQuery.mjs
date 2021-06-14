34d8f47ce308560d89335263a298b4459bd13dc4
Joan Andrés <andres6936@live.com>
2021-06-14 16:04:22 -0500

    Implemented BrowserQuery

--- src/main/js/search/BrowserQuery.mjs
+++ src/main/js/search/BrowserQuery.mjs
@@ -1,3 +1,28 @@
-class BrowserQuery {
+import puppeteer from "puppeteer";
 
+export class BrowserQuery {
+    #browser = undefined;
+    #page = undefined;
+
+    async constructor() {
+        this.#browser = await puppeteer.launch({
+            headless: false,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+        this.#page = await this.#browser.newPage();
+    }
+
+    async getHtmlByQuery(query) {
+        await this.#page.goto('https://google.com');
+        await this.#page.click('[name=q]');
+        await this.#page.keyboard.type(query);
+        await this.#page.keyboard.press('Enter');
+        await this.#page.waitForSelector('h3.LC20lb', {timeout: 2500});
+        await this.#page.click('h3.LC20lb');
+        return await this.#page.content();
+    }
+
+    async close() {
+        this.#browser.close();
+    }
 }
