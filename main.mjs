7e21b232f78f78e575f16efb43558d5afc683947
Joan Andrés <andres6936@live.com>
2021-06-14 16:06:36 -0500

    Use of BrowserQuery Service

--- main.mjs
+++ main.mjs
@@ -1,27 +1,14 @@
-import puppeteer from "puppeteer";
 import fs from 'fs';
+import {BrowserQuery} from "./src/main/js/search/BrowserQuery.mjs";
 
 const url = 'Simea Dinas site:http://scienti.colciencias.gov.co';
 
-async function getHtmlFromQuery(url) {
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-    const page = await browser.newPage();
-    await page.goto('https://google.com');
-    await page.click('[name=q]');
-    await page.keyboard.type(url);
-    await page.keyboard.press('Enter');
-    await page.waitForSelector('h3.LC20lb', {timeout: 2500});
-    await page.click('h3.LC20lb');
-    return await page.content();
-}
-
 (async () => {
-    const html = await getHtmlFromQuery(url);
+    const browserQuery = await new BrowserQuery();
+    const html = await browserQuery.getHtmlByQuery(url);
     fs.writeFile('Output.html', html, (error) => {
         if (error) return console.log(error);
         console.log('Writing file > Output.html');
     });
+    browserQuery.close();
 })()
