f8cd326fd1338f5f238f50de3bda45c159fb6ed3
Joan Andrés <andres6936@live.com>
2021-06-14 14:11:03 -0500

    Get content from query

--- main.mjs
+++ main.mjs
@@ -1,8 +1,9 @@
 import puppeteer from "puppeteer";
+import fs from 'fs';
 
 const url = 'Simea Dinas site:http://scienti.colciencias.gov.co';
 
-(async () => {
+async function getHtmlFromQuery(url) {
     const browser = await puppeteer.launch({
         headless: false,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
@@ -13,7 +14,14 @@ const url = 'Simea Dinas site:http://scienti.colciencias.gov.co';
     await page.keyboard.type(url);
     await page.keyboard.press('Enter');
     await page.waitForSelector('h3.LC20lb', {timeout: 2500});
-    await page.evaluate(() => {
-        document.querySelectorAll('h3.LC20lb').item(0).click();
-    })
+    await page.click('h3.LC20lb');
+    return await page.content();
+}
+
+(async () => {
+    const html = await getHtmlFromQuery(url);
+    fs.writeFile('Output.html', html, (error) => {
+        if (error) return console.log(error);
+        console.log('Writing file > Output.html');
+    });
 })()
