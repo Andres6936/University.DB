c3f45f2a054ea33d7192d87f6ed48d8def79270d
Joan Andrés <andres6936@live.com>
2021-06-15 07:10:09 -0500

    The method startUp return instance of object

--- src/main/js/DatabaseManagerService.mjs
+++ src/main/js/DatabaseManagerService.mjs
@@ -19,6 +19,7 @@ export class DatabaseManagerService {
         await this.#client.connect();
         // Establish and verify connection
         this.#connection = await this.#client.db('University');
+        return this;
     }
 
     /**
