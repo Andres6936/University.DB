6431b15243e885fcd055980687e3e3f43bd4a32c
Joan Andrés <andres6936@live.com>
2021-06-16 21:11:43 -0500

    Merge Slash

--- src/main/js/scrapper/AcademicInformation.mjs
+++ src/main/js/scrapper/AcademicInformation.mjs
@@ -6,6 +6,7 @@ export class AcademicInformation {
         let nodes = new KeyValueNodeExtractor(node).toArray();
         for (const node of nodes) {
             node.first = node.second.substr(0, node.second.indexOf('\n'));
+            node.first = NormalizeStringService.mergeSlash(node.first);
             let value = NormalizeStringService.removeExtraSpaces(node.second)
             value = NormalizeStringService.removeLineBreak(value);
             node.second = value;
